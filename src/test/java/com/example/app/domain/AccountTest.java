f419dc8f9efed4a318a28435b994f06742e74a0d
Vassilis <bzafiris@gmail.com>
2020-10-19 15:40:15 +0300

    Added sample code, migrated to Java 11

---
+++ src/test/java/com/example/app/domain/AccountTest.java
@@ -0,0 +1,14 @@
+package com.example.app.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AccountTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
