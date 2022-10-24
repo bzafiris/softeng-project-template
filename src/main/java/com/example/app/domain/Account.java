cd5b4bc450ca28af99f5e71f1e9dd5dc67ebe566
Vassilis Zafeiris <bzafiris@gmail.com>
2022-10-24 20:39:21 +0300

    Added sample jpa code

--- src/main/java/com/example/app/domain/Account.java
+++ src/main/java/com/example/app/domain/Account.java
@@ -1,5 +1,37 @@
 package com.example.app.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
 public class Account {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "number", length = 10)
+    private String accountNumber;
+
+    public Account() {
+    }
+
+    public Account(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    
 }
--- src/test/java/com/example/app/domain/AccountTest.java
+++ src/test/java/com/example/app/domain/AccountTest.java
@@ -2,13 +2,50 @@ package com.example.app.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.example.app.persistence.JPAUtil;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 
 public class AccountTest {
 
+
+	private EntityManager em;
+
+	@BeforeEach
+	public void setup(){
+		em = JPAUtil.getCurrentEntityManager();
+	}
+
+	@AfterEach
+	public void tearDown(){
+		em.close();
+	}
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void saveAccount() {
+
+		Account acc = new Account("12345");
+		persistAccount(acc);
+		assertNotNull(acc.getId());
+		em.clear();
+
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		Account savedAccount = em.find(Account.class, acc.getId());
+		assertEquals("12345", savedAccount.getAccountNumber());
+		tx.commit();
+
+	}
+
+	private void persistAccount(Account account){
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		em.persist(account);
+		tx.commit();
 	}
 
 }
